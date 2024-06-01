259a5a041e3bf4c94cec6e161fb0532b25a781e2
bapanapalligopi <bapanapalligopi7@gmail.com>
2024-06-01 19:26:42 +0530

    version-0.0.1

---
+++ cryptopricefinder/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Copyright @ 2024, CryptoPriceFinder - All rights Reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
